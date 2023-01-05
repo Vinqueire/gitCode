831f21478cec3180678f76ca62da599635ee4915
Frolof192@gmail.com <117796287+Vinqueire@users.noreply.github.com>
2023-01-05 18:31:56 +0200

    I added an image to the inner html, so now it displays both text and an image

--- code/app.js
+++ code/app.js
@@ -2,11 +2,10 @@ const textContainer = document.getElementById("textContainer");
 
 function pokazywacztekstu() {
   if (textContainer.innerHTML === "") {
-    textContainer.innerHTML =
-      "Woah, you actually clicked me! ^_^ <br>You can click me again to make me disappear :O";
+    textContainer.innerHTML = `Woah, you actually clicked me! ^_^ <br>You can click me again to make me disappear :O <br> <img src="continue.png" alt="png">`;
   } else if (
     textContainer.innerHTML ===
-    "Woah, you actually clicked me! ^_^ <br>You can click me again to make me disappear :O"
+    `Woah, you actually clicked me! ^_^ <br>You can click me again to make me disappear :O <br> <img src="continue.png" alt="png">`
   ) {
     textContainer.innerHTML = "";
   }
