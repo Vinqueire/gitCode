95dc66b10f708e31ce6a8b72e2a4939b3a8880ae
Jourvence <Frolof192@gmail.com>
2023-01-06 10:06:06 +0100

    I added a second function to the on click of the button, and made it display alongside the text displayed, i also changed the colours of the text and played around with it a little bit, I added a bit of css to centre it because it needed some work when the second function came around, i also added webkit stroke (outline to the text)

--- code/app.js
+++ code/app.js
@@ -2,11 +2,18 @@ const textContainer = document.getElementById("textContainer");
 
 function pokazywacztekstu() {
   if (textContainer.innerHTML === "") {
-    textContainer.innerHTML = `Woah, you actually clicked me! ^_^ <br>You can click me again to make me disappear :O <br> <img src="continue.png" alt="png">`;
-  } else if (
-    textContainer.innerHTML ===
-    `Woah, you actually clicked me! ^_^ <br>You can click me again to make me disappear :O <br> <img src="continue.png" alt="png">`
-  ) {
+    textContainer.innerHTML = `<span id="liniatxt1">Woah, you actually clicked me! ^_^</span> <br><span id ="liniatxt2">You can click me again to make me disappear :O</span> <br><span id = "liniatxt3">Or you can even click on the image to change it :O!!!</span> <img id="zdj1" src="continue.png" alt="png">`;
+  } else if (textContainer.innerHTML !== "") {
     textContainer.innerHTML = "";
+  }
+}
+
+const emojiContainer = document.getElementById("emojiContainer");
+
+function stworzramke() {
+  if (emojiContainer.innerHTML === "") {
+    emojiContainer.innerHTML = `🔥🔥🔥🔥🔥🔥🔥`;
+  } else if (emojiContainer.innerHTML === `🔥🔥🔥🔥🔥🔥🔥`) {
+    emojiContainer.innerHTML = "";
   }
 }
