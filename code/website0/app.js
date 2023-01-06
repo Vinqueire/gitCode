e65e2d2e9f8c4ec12a1a5097f6f79de5de5f970f
Jourvence <Frolof192@gmail.com>
2023-01-06 15:06:55 +0100

    I finished the first website, created a new directory within the code directory and created files within the new directory, also i grouped the previous files in a directory called website0 and created a directory website1 for a new website that will be a carousell slider

--- code/website0/app.js
+++ code/website0/app.js
@@ -2,7 +2,7 @@ const textContainer = document.getElementById("textContainer");
 
 function pokazywacztekstu() {
   if (textContainer.innerHTML === "") {
-    textContainer.innerHTML = `<span id="liniatxt1">Woah, you actually clicked me! ^_^</span> <br><span id ="liniatxt2">You can click me again to make me disappear :O</span> <br><span id = "liniatxt3">Or you can even click on the image to change it :O!!!</span> <img id="zdj1" src="continue.png" alt="png">`;
+    textContainer.innerHTML = `<span id="liniatxt1">Woah, you actually clicked me! ^_^</span> <br><span id ="liniatxt2">You can click me again to make me disappear :O</span> <br><span id = "liniatxt3">Or you can even click on the image to change it :O!!!</span> <img id="zdj1" src="hdimg0.jpg" alt="jpg">`;
   } else if (textContainer.innerHTML !== "") {
     textContainer.innerHTML = "";
   }
