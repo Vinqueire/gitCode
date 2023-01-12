6e829d8c9ab7b090d934204e1e801a047f4115ef
Frolof192@gmail.com <117796287+Vinqueire@users.noreply.github.com>
2023-01-12 09:37:39 +0200

    I did A little bit of PROGRESS in my shapes skill

--- code/cs50/training/shapes.c
+++ code/cs50/training/shapes.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
-
 int main(void)
 {
     int i,j;
 
-    for (i = 1; i <= 5; i++)
+    for (i = 5; i >= 1; i--)
     {
-        for (j = 1; j <= i; j++)
+        for (j = 1; j <=i; j++)
         {
-            printf("%c",i + 64);
+            printf("*");
         }
         printf("\n");
     }
